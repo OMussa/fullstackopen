daa37bddc180d5d0588086833baec881d5ebdde8
OMussa <mussa_o@icloud.com>
2023-03-11 18:07:33 -0600

    exercise 2.8

--- part2/src/App.js
+++ part2/src/App.js
@@ -5,38 +5,48 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
-  const addName = (event) =>{
+  const [numbers,setNumber] = useState('')
+  
+
+ 
+  const addNameNumber = (event) =>{
     event.preventDefault() //prevents page from refreshing
     if(persons.find(person=> person.name == newName)){
       alert(`${newName} is already added to the phonebook`)
+    }else if(persons.find(person=> person.number == numbers)){
+      alert(`${numbers} is already added to the phonebook`)
     }else{
     const nameObject = {
       name: newName,
+      number: numbers,
       id: persons.length + 1
     }
     setPersons(persons.concat(nameObject))
     setNewName('')
   }
+
   }
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNumber(event.target.value)
   }
  
   return (
     <div>
       <h2>Phonebook</h2>
       <form >
-        <div>
-          name: <input  onChange={handleNoteChange} />
-        </div>
-        <div>
-          <button type="submit" onClick={addName}>add</button>
-        </div>
+        <div> name: <input  onChange={handleNoteChange} /> </div> 
+        <div> number: <input onChange={handleNumberChange} /> </div>
+        <div> <button type="submit" onClick={addNameNumber}>add</button> </div>
       </form>
       <h2>Numbers</h2>
 <div>
-  {persons.map(person => <p key={person.id}>{person.name}</p>)}
+  {persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
+  
 </div>
       
     </div>
