f7efd458964113118bda7dc447bd0f73f38334a0
OMussa <mussa_o@icloud.com>
2023-04-25 12:21:51 -0500

    exercise 3.3-3.4

--- part3/index.js
+++ part3/index.js
@@ -30,7 +30,7 @@ app.get('/api/persons',(request,response)=>{
 })
 
 app.get('/api/persons/:id', (request,response) =>{
-    const id = request.params.id
+    const id = Number(request.params.id)
     const numberId = persons.find(person => person.id == id)
     if(numberId){
         response.json(numberId)
@@ -51,6 +51,12 @@ app.get('/info',(request,response)=>{
         
         )
       
+})
+
+app.delete('/api/persons/:id', (request,response) =>{
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
 })
 
 const PORT = 3001
